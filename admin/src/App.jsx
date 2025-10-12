c87b4bb3f9cdba053bd315b6309768d6bf60228f
vtalesha <vipultalesha85@gmail.com>
2025-10-12 20:22:11 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -9,7 +9,7 @@ import Login from './components/Login'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-export const backendUrl = import.meta.env.VITE_BACKEND_URL
+export const backendUrl = "https://trendkart-e-commerce-backend-6gn4.onrender.com"
 export const currency = ' ₹'
 
 const App = () => {
