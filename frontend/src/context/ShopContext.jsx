392f1aaba2142d4128e6c9be6cedd3491950475c
vtalesha <vipultalesha85@gmail.com>
2025-10-12 20:21:36 +0530

    Update ShopContext.jsx

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -9,7 +9,7 @@ const ShopContextProvider = (props) => {
 
     const currency = '₹';
     const delivery_fee = 10;
-    const backendUrl = import.meta.env.VITE_BACKEND_URL
+    const backendUrl = "https://trendkart-e-commerce-backend-6gn4.onrender.com"
     const [search, setSearch] = useState('');
     const [showSearch, setShowSearch] = useState(false);
     const [cartItems, setCartItems] = useState({});
